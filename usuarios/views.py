9f09fb41e935c331455f12f25550716423895a70
Gleison <gleison04lemos@gmail.com>
2022-07-13 15:47:21 -0300

    Refatoração e mensagens ao usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from receitas.models import Receita
 
 def cadastro(request):
@@ -11,52 +11,62 @@ def cadastro(request):
         senha = request.POST['password']
         senha2 = request.POST['password2']
 
-        if not nome.strip():
-            print('Nome não pode ser vazio.')
+        if campo_vazio(nome):
+            messages.error(request, 'Campo nome está vazio.')
             return redirect('cadastro')
 
-        if not email.strip():
-            print('Endereço de email inválido!')
+        if campo_vazio(email):
+            messages.error(request, 'Endereço de email inválido!')
             return redirect('cadastro')
 
-        if senha != senha2:
-            print('As senhas não coincidem.')
+        if senhas_diferentes(senha, senha2):
+            messages.error(request, 'As senhas não coincidem!')
             return redirect('cadastro')
 
         if User.objects.filter(email=email).exists():
-            print('Usuário já cadastrado')
+            messages.error(request, 'Email já cadastrado.')
             return redirect('cadastro')
-            
+
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Email já cadastrado.')
+            return redirect('cadastro')
+
         user = User.objects.create_user(username=nome, email=email, password=senha)
         user.save()
-        print('Usuário cadastrado com sucesso')
+        messages.success(request, 'Cadastro realizado com sucesso!')
         return redirect('login')
         
     else:
         return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+
     if request.method == 'POST':
         email = request.POST['email']
         senha = request.POST['senha']
-        if email == '' or senha == '':
-            print('Não pode ter campos vazios.')
+
+        if campo_vazio(email) or campo_vazio(senha):
+            messages.error(request, 'Preencha os campos corretamente.')
             return redirect('login')
         print(email, senha)
+
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
             user = auth.authenticate(request, username=nome, password=senha)
             if user is not None:
                 auth.login(request, user)
-                print('Login realizado com sucesso')
+                messages.success(request, 'Login realizado com sucesso!')
                 return redirect('dashboard')
+
     return render(request, 'usuarios/login.html')
 
 def logout(request):
+
     auth.logout(request)
     return redirect('index')
 
 def dashboard(request):
+
     if request.user.is_authenticated:
         id = request.user.id
         receitas = Receita.objects.order_by('-date_receita').filter(pessoa=id)
@@ -69,6 +79,7 @@ def dashboard(request):
         return redirect('index')
 
 def cria_receita(request):
+
     if request.method == 'POST':
         nome_receita = request.POST['nome_receita']
         ingredientes = request.POST['ingredientes']
@@ -84,5 +95,12 @@ def cria_receita(request):
         rendimento=rendimento,categoria=categoria, foto_receita=foto_receita)
         receita.save()
         return redirect('dashboard')
+
     else:
         return render(request, 'usuarios/cria_receita.html')
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+def senhas_diferentes(senha, senha2):
+    return senha != senha2
