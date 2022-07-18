99e6ee6b8ce5587506d1fd2c8fd86c636e8e9642
Gleison <gleison04lemos@gmail.com>
2022-07-18 11:50:14 -0300

    editando e atualizando formulários

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,9 @@
+import re
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
+from receitas.views import receita
 
 def cadastro(request):
 
@@ -104,8 +106,28 @@ def deleta_receita(request, receita_id):
     receita.delete
     return redirect('dashboard')
 
+def edita_receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_a_editar = { 'receita': receita}
+    return render(request, 'usuarios/edita_receita.html', receita_a_editar)
+
 def campo_vazio(campo):
     return not campo.strip()
 
 def senhas_diferentes(senha, senha2):
     return senha != senha2
+
+def atualiza_receita(request):
+    if request.method == 'POST':
+        receita_id = request.POST['receita_id']
+        r = Receita.objects.get(pk=receita_id)
+        r.nome_receita = request.POST['nome_receita']
+        r.ingredientes = request.POST['ingredientes']
+        r.modo_preparo = request.POST['modo_preparo']
+        r.tempo_preparo = request.POST['tempo_preparo']
+        r.rendimento = request.POST['rendimento']
+        r.categoria = request.POST['categoria']
+        if 'foto_receita' in request.FILES:
+            r.foto_receita = request.FILES['foto_receita']
+        r.save()
+        return redirect('dashboard')
